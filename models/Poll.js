8f6e0d04e2f002a95634952713ef61ea41ab5adf
Fausto Vazquez <soleogrim@gmail.com>
2026-07-20 14:46:40 -0400

    Feat/poll model (#11)
    * feat: poll model
    
    ---------
    
    Co-authored-by: Fausto Vazquez <faustovazquez.005@gmail.com>

---
+++ models/Poll.js
@@ -0,0 +1,22 @@
+const { DataTypes, Sequelize } = require('sequelize')
+const db = require('../db')
+
+const PollModel = db.define('poll', {
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false, 
+        validate: {
+            notEmpty: true
+        }
+    },
+    description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            notEmpty: true
+        },
+    }
+
+})
+
+module.exports = PollModel
