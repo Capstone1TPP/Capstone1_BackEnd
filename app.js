9ac8654a488fc00b1f411071c4880944bcd71655
Yangmei (Amy) Lu <109566558+AmyLuSunshine@users.noreply.github.com>
2026-07-22 13:19:38 -0400

    update the post Poll-Vote route (update my db connection)

--- app.js
+++ app.js
@@ -1,65 +1,73 @@
-const express = require('express')
-const { PollModel, OptionModel, VoteModel, db} = require('./models')
+const express = require("express");
+const { PollModel, OptionModel, VoteModel, db } = require("./models");
 
-const PORT = 4000
+const PORT = 4000;
 
-const app = express()
+const app = express();
 
+app.use(express.json());
 
-app.use(express.json())
+app.get("/health", (req, res) => {
+  res.json({ status: "It works" });
+});
 
-app.get('/health', (req,res) => {
-    res.json({status: 'It works'})
-})
+app.get("/polls", async (req, res) => {
+  const polls = await PollModel.findAll();
+  console.log(polls.poll);
 
-app.get('/polls', async (req,res) => {
-    const polls = await PollModel.findAll()
-    console.log(polls.poll)
+  res.status(200).json(polls);
+});
 
-    res.status(200).json(polls)
-})
+app.get("/polls/:id", async (req, res) => {
+  const id = Number(req.params.id);
 
-app.get('/polls/:id', async (req,res) => {
-    const id = Number(req.params.id)
+  const singlePoll = await PollModel.findByPk(id, {
+    include: {
+      model: OptionModel,
+      include: [VoteModel],
+    },
+  });
+  if (!singlePoll) {
+    return res.sendStatus(404);
+  }
+  res.status(200).json(singlePoll);
+});
 
-    const singlePoll = await PollModel.findByPk(id, {
-        include: {
-            model: OptionModel,
-            include: [VoteModel]
-    }})
-    if(!singlePoll){
-        return res.sendStatus(404);
-    }
-    res.status(200).json(singlePoll)
+app.post("/polls", async (req, res) => {
+  const { title, description, options } = req.body;
 
+  const newPoll = await PollModel.create({ title, description });
 
-})
+  const createdOptions = options.map(async (option) => {
+    await OptionModel.create({ text: option.text, pollId: newPoll.id });
+  });
 
-app.post('/polls', async(req,res) => {
-    const {title, description, options} = req.body
+  res.status(200).json(newPoll);
+});
 
-    const newPoll = await PollModel.create({title, description})
+app.post("/polls/:id/vote", async (req, res) => {
+  //optionId UUID for the vote.
+  const { optionId } = req.body;
 
+  const id = Number(req.params.id);
+  const poll = await PollModel.findByPk(id);
 
-    const createdOptions = options.map(async(option) => {
+  if (!poll) {
+    res.status(404).json("no poll");
+  }
 
-        await OptionModel.create({text: option.text , pollId: newPoll.id})
-    })
+  const vote = await VoteModel.create({ optionId });
 
-    res.status(200).json(newPoll)
-})
-
+  res.status(201).json(vote);
+});
 
 async function startApp() {
-    await db.sync()
-    .then(() => {
-        console.log('Connection successful')
-        app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`))
-    })
-
+  await db.sync().then(() => {
+    console.log("Connection successful");
+    app.listen(PORT, () =>
+      console.log(`Server running on port http://localhost:${PORT}`),
+    );
+  });
 }
 
-startApp()
-
-
-
+startApp();
--- db.js
+++ db.js
@@ -1,8 +1,7 @@
-const { Sequelize } = require('sequelize')
+const { Sequelize } = require("sequelize");
 
-const dbConnection  = new Sequelize(
-    "postgres://localhost:5432/PollingApp"
-)
+const dbConnection = new Sequelize(
+  "postgres://postgres:Luyangmei123@localhost:5432/PollingApp",
+);
 
-
-module.exports = dbConnection 
+module.exports = dbConnection;
